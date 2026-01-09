024a97f3e611475215f3cd2348a87e61811fbfeb
Guzmán González de Riancho <guzmangriancho@gmail.com>
2026-01-09 12:19:23 +0100

    initialize project

---
+++ src/test/java/com/qaracter/smartbank/SmartbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qaracter.smartbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
