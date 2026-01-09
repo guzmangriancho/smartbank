a4fe9740f981bd6772b75c57f2bd56da7fc20f49
Guzmán González de Riancho <guzmangriancho@gmail.com>
2026-01-09 13:39:34 +0100

    feat: tickets controller

--- src/main/java/com/qaracter/smartbank/controller/SupportTicketController.java
+++ src/main/java/com/qaracter/smartbank/controller/SupportTicketController.java
@@ -1,4 +1,36 @@
 package com.qaracter.smartbank.controller;
 
+import com.qaracter.smartbank.model.SupportTicket;
+import com.qaracter.smartbank.service.SupportTicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tickets")
 public class SupportTicketController {
+
+    @Autowired
+    private SupportTicketService service;
+
+    @PostMapping
+    public SupportTicket createTicket(@RequestBody SupportTicket ticket) {
+        return service.createTicket(ticket);
+    }
+
+    @PutMapping("/{id}/assign")
+    public SupportTicket assignTicket(@PathVariable Long id, @RequestParam String employee) {
+        return service.assignTicket(id, employee);
+    }
+
+    @PutMapping("/{id}/status")
+    public SupportTicket updateTicketStatus(@PathVariable Long id, @RequestParam String status) {
+        return service.updateTicketStatus(id, status);
+    }
+
+    @GetMapping("/open")
+    public List<SupportTicket> getOpenTickets() {
+        return service.getOpenTickets();
+    }
 }
