8d773b0e8ef194c6792fa56965c0e4dfe8862bb1
Guzmán González de Riancho <guzmangriancho@gmail.com>
2026-01-09 13:05:35 +0100

    feat: customer service and controller

--- src/main/java/com/qaracter/smartbank/controller/CustomerController.java
+++ src/main/java/com/qaracter/smartbank/controller/CustomerController.java
@@ -1,4 +1,30 @@
 package com.qaracter.smartbank.controller;
 
+import com.qaracter.smartbank.model.Customer;
+import com.qaracter.smartbank.service.CustomerService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
 public class CustomerController {
+
+    @Autowired private CustomerService service;
+
+    @PostMapping
+    public Customer create(@RequestBody Customer customer) {
+        return service.createCustomer(customer);
+    }
+
+    @GetMapping
+    public List<Customer> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Customer get(@PathVariable Long id) {
+        return service.getById(id);
+    }
 }
--- src/main/java/com/qaracter/smartbank/service/CustomerService.java
+++ src/main/java/com/qaracter/smartbank/service/CustomerService.java
@@ -1,4 +1,29 @@
 package com.qaracter.smartbank.service;
 
+import com.qaracter.smartbank.exception.CustomerNotFoundException;
+import com.qaracter.smartbank.model.Customer;
+import com.qaracter.smartbank.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CustomerService {
+    @Autowired
+    private CustomerRepository repo;
+
+    public Customer createCustomer(Customer customer) {
+        return repo.save(customer);
+    }
+
+    public List<Customer> getAll() {
+        return repo.findAll();
+    }
+
+    public Customer getById(Long id) {
+        Customer customer = repo.findById(id);
+        if (customer == null) throw new CustomerNotFoundException("Customer not found");
+        return customer;
+    }
 }
