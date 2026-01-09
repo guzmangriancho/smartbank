7e6a483f2519b1aea1cc81ceb85b468db1541457
Guzmán González de Riancho <guzmangriancho@gmail.com>
2026-01-09 13:00:23 +0100

    feat: customer repository

--- src/main/java/com/qaracter/smartbank/repository/CustomerRepository.java
+++ src/main/java/com/qaracter/smartbank/repository/CustomerRepository.java
@@ -1,4 +1,31 @@
 package com.qaracter.smartbank.repository;
 
+import com.qaracter.smartbank.model.Customer;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
 public class CustomerRepository {
+    private final Map<Long, Customer> customers = new HashMap<>();
+    private long idCounter = 1;
+
+    public Customer save(Customer c) {
+        if (c.getId() == null) {
+            c.setId(idCounter++);
+        }
+        customers.put(c.getId(), c);
+        return c;
+    }
+
+    public List<Customer> findAll() {
+        return new ArrayList<>(customers.values());
+    }
+
+    public Customer findById(Long id) {
+        return customers.get(id);
+    }
 }
